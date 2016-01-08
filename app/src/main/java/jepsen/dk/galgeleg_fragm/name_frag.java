c56ff854fc234fc4f4d70ed35c304a722153cee6
Jacobjepsen27 <Jacob.worck@gmail.com>
2016-01-08 12:25:14 +0100

    Tilføjelse: PreferenceManager - App'en gemmer brugerens sidst valgte brugernavn

--- app/src/main/java/jepsen/dk/galgeleg_fragm/name_frag.java
+++ app/src/main/java/jepsen/dk/galgeleg_fragm/name_frag.java
@@ -1,16 +1,21 @@
 package jepsen.dk.galgeleg_fragm;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.parse.ParseObject;
+
+import java.util.prefs.Preferences;
 
 
 /**
@@ -19,11 +24,13 @@ import com.parse.ParseObject;
 public class name_frag extends Fragment implements View.OnClickListener{
 
     public ViewGroup rod;
-
     private Button gem;
     private TextView name;
     private String navn;
     private long score;
+
+    SharedPreferences sp;
+    String userNameString;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,7 +38,12 @@ public class name_frag extends Fragment implements View.OnClickListener{
         // Inflate the layout for this fragment
         rod = (ViewGroup) inflater.inflate(R.layout.fragment_name_frag, container, false);
 
-        name = (TextView) rod.findViewById(R.id.highscoreName);
+        sp = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        userNameString = sp.getString("minNøgle", "Brugernavn");
+
+
+        name = (EditText) rod.findViewById(R.id.highscoreName);
+        name.setText(userNameString);
 
         gem = (Button) rod.findViewById(R.id.saveButton);
         gem.setOnClickListener(this);
@@ -43,8 +55,11 @@ public class name_frag extends Fragment implements View.OnClickListener{
     public void onClick(View v) {
 
         if(v==gem){
+
             navn = name.getText().toString();
             score = SingleTon.getGlInstance().getScore();
+
+            sp.edit().putString("minNøgle", navn).commit();
 
             ParseObject gameScore = new ParseObject("HiScore");
             gameScore.put("navn", navn);
