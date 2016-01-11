b353855ddef01e0288c924bc569206ae5ddedefa
Jacobjepsen27 <Jacob.worck@gmail.com>
2016-01-11 12:14:15 +0100

    Rettelse: "Se Highscore" benytter nu ét listview istedet for to.

--- app/src/main/java/jepsen/dk/galgeleg_fragm/highscore_frag.java
+++ app/src/main/java/jepsen/dk/galgeleg_fragm/highscore_frag.java
@@ -9,15 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.widget.TextView;
 
 
 /**
@@ -31,23 +23,38 @@ public class highscore_frag extends Fragment {
     ArrayAdapter<String> adapter1, adapter2;
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rod = (ViewGroup) inflater.inflate(R.layout.fragment_highscore_frag, container, false);
 
-        left = (ListView) rod.findViewById(R.id.highViewLeft);
-        right = (ListView) rod.findViewById(R.id.highViewRight);
+        ArrayAdapter adapter = new ArrayAdapter(this.getContext(), R.layout.listview_egetlayout, R.id.listeelem_navn, SingleTon.navnShow) {
+            public View getView(int position, View cachedView, ViewGroup parent){
+                View view = super.getView(position, cachedView, parent);
 
+                TextView score = (TextView) view.findViewById(R.id.listeelem_score);
+                score.setText(SingleTon.scoreShow[position]);
 
+                return view;
+            }
+        };
 
+        ListView listView = (ListView) rod.findViewById(R.id.highViewLeft);
+        listView.setAdapter(adapter);
 
-        adapter1 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, SingleTon.navnShow);
-        left.setAdapter(adapter1);
-
-        adapter2 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, SingleTon.scoreShow);
-        right.setAdapter(adapter2);
+//        left = (ListView) rod.findViewById(R.id.highViewLeft);
+//        right = (ListView) rod.findViewById(R.id.highViewRight);
+//
+//
+//
+//
+//        adapter1 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, SingleTon.navnShow);
+//        left.setAdapter(adapter1);
+//
+//        adapter2 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, SingleTon.scoreShow);
+//        right.setAdapter(adapter2);
 
         return rod;
     }
