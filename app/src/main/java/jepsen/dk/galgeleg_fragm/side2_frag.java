46481267a63a85a5e08f82594c1965ddf52882b6
camillaolsen <camilla.braae.olsen1@gmail.com>
2016-01-12 10:01:38 +0100

    Ryddet op i kode fra side2 frag

--- app/src/main/java/jepsen/dk/galgeleg_fragm/side2_frag.java
+++ app/src/main/java/jepsen/dk/galgeleg_fragm/side2_frag.java
@@ -23,67 +23,40 @@ import java.util.ArrayList;
 public class side2_frag extends Fragment implements View.OnClickListener{
 
     private ImageView img;
-    private EditText editTxt;
     private TextView txt;
-    private TextView bogstaver;
-    private String bString;
     private String bogstav;
-    // private Button bogstav;
     private ViewGroup rod;
     private int[] knap = {R.id.A, R.id.B, R.id.C, R.id.D, R.id.E, R.id.F, R.id.G, R.id.H, R.id.I, R.id.J, R.id.K, R.id.L,
             R.id.M, R.id.N, R.id.O, R.id.P, R.id.Q, R.id.R, R.id.S, R.id.T, R.id.U, R.id.V, R.id.W, R.id.X, R.id.Y, R.id.Z, R.id.Æ, R.id.Ø, R.id.Å};
-    private ArrayList <Button> knapper = new ArrayList<Button>();
     Button btn;
-
-
 
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
 
         rod = (ViewGroup) i.inflate(R.layout.side2_frag, container, false);
 
-
-
         if (savedInstanceState == null) {
-            Log.d("HALLOOOOOO", "printer - IKKE");
             img = (ImageView) rod.findViewById(R.id.mainImgImageView);
-           // editTxt = (EditText) rod.findViewById(R.id.bogstavEditText);
-           // editTxt.setOnKeyListener(this);
             txt = (TextView) rod.findViewById(R.id.ordTextView);
             txt.setText(SingleTon.getGlInstance().getSynligtOrd());
-            //bogstaver = (TextView) rod.findViewById(R.id.tidligereGætTextView2);
-            //bString = bogstaver.getText().toString();
             for (int j = 0; j < knap.length; j++) {
                 btn = (Button) rod.findViewById(knap[j]);
                 btn.setOnClickListener(this);
-                knapper.add(btn);
             }
-
-            Log.d("FØRBOGSTAVER", "printer - IKKE");
         }
-
         return rod;
     }
 
-
-
     @Override
     public void onClick(View v) {
-        int id = v.getId();
-        //bogstav = Integer.toString(letter);
-        Log.d("JJJJ", "HEJ");
 
         if(v instanceof Button){
-            Log.d("KKKK", "LOL");
            bogstav = ((Button) v).getText().toString().toLowerCase();
             v.setSelected(true);
-
+            v.setEnabled(false);
     }
 
-        //String bogstav = editTxt.getText().toString().toLowerCase();
-                //editTxt.setText(""); // Nulstiller indtastningsfeltet (fjerner bogstav)
-
-                // Gæt på et bogstav og håndter fejlsituationer (ugyldige gæt)
+              // Gæt på et bogstav og håndter fejlsituationer (ugyldige gæt)
                 try {
                     SingleTon.getGlInstance().gætBogstav(bogstav);
                 } catch (Exception e){
@@ -95,12 +68,6 @@ public class side2_frag extends Fragment implements View.OnClickListener{
                 //SingleTon.getGlInstance().setStatus(true);
                 //SingleTon.getGlInstance().setHighscore(100);
 
-                // Opdater listen af gættede bogstaver
-                String bogstavGæt = "";
-                for (String s  : SingleTon.getGlInstance().getBrugteBogstaver()){
-                    bogstavGæt += s + ", " ;
-                }
-                //bogstaver.setText(bString + bogstavGæt);
 
                 // Opdater det synlige ord
                 txt.setText(SingleTon.getGlInstance().getSynligtOrd());
