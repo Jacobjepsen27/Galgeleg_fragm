74d1d696651e5ec86821bc984f56c5da6e4f9c4e
Jacobjepsen27 <Jacob.worck@gmail.com>
2016-01-15 16:13:30 +0100

    Tilføjelse: Context Menu til ordliste. Funktionalitet ikke implementeret

--- app/src/main/java/jepsen/dk/galgeleg_fragm/ordListe_activity.java
+++ app/src/main/java/jepsen/dk/galgeleg_fragm/ordListe_activity.java
@@ -2,10 +2,16 @@ package jepsen.dk.galgeleg_fragm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class ordListe_activity extends AppCompatActivity {
+public class ordListe_activity extends AppCompatActivity{
 
     private ListView listView;
     private ArrayAdapter<String> adapter;
@@ -19,6 +25,42 @@ public class ordListe_activity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.ordListe);
         listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "LAAAAANGT KLIK", Toast.LENGTH_SHORT).show();
+                openContextMenu(listView);
+                return true;
+            }
+        });
+        registerForContextMenu(listView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.context_menu, menu);
+        if (v.getId()==R.id.ordListe){
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+            menu.setHeaderTitle("Redigering");
+            menu.add("Edit");
+            menu.add("Slet");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.test_context:
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
 
     }
 }
